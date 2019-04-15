97af6d0a00e0a7d19e6bb7477bbc19f18d92ca7c
Mike Lock <michael.lockwitz@consensys.net>
2019-04-15 14:43:02 -0600

    Removing old version of node from CI check.

--- example/src/App.js
+++ example/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-
-import { Text } from "rimble-ui";
 import { ConnectionBanner } from "rimble-web3-components";
+import { Text } from "rimble-ui";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Text>Connection Banner</Text>
-        <ConnectionBanner requiredNetwork={3} curerentNetwork={4} />
+        <ConnectionBanner currentNetwork={3} requiredNetwork={4} />
       </div>
     );
   }

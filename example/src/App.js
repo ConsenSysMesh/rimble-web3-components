eb501bcd19302704f5fde79aa6c06bd6d548cae8
Mike Lock <michael.lockwitz@consensys.net>
2019-04-15 14:38:13 -0600

    Fixing failing test, adding component snapshot.

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
+import { Text } from "rimble-ui";
 import { ConnectionBanner } from "rimble-web3-components";
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Text>Connection Banner</Text>
         <ConnectionBanner requiredNetwork={3} curerentNetwork={4} />
       </div>
     );
--- src/test.js
+++ src/test.js
@@ -1,7 +1,15 @@
-import ExampleComponent from './'
+import React from "react";
+import { configure, shallow, render, mount } from "enzyme";
+import renderer from "react-test-renderer";
+import Adapter from "enzyme-adapter-react-16";
+import "jest-styled-components";
+configure({ adapter: new Adapter() });
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+import ConnectionBanner from "./ConnectionBanner";
+
+describe("ConnectionBanner", () => {
+  it("matches default snapshot", () => {
+    const component = render(<ConnectionBanner />);
+    expect(component).toMatchSnapshot();
+  });
+});

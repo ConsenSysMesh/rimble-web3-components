c5ff513c1531a563f97232055b6740728a3b29dc
Mike Lock <michael.lockwitz@consensys.net>
2019-07-10 15:10:33 -0600

    Refactored to remove unnecessary UI.

--- packages/EthConverter/src/index.js
+++ packages/EthConverter/src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-  Heading,
-  Box,
-  Flex,
-  Field,
-  Input,
-  Select,
-  Card,
-  Text,
-} from 'rimble-ui';
-
+import { Box, Tooltip } from 'rimble-ui';
 import Currencies from './CurrencyMap';
 
 class EthConverter extends React.Component {
   state = {
-    baseValue: 1,
     loading: true,
     conversion: [],
     lastUpdate: '',
@@ -26,14 +14,12 @@ class EthConverter extends React.Component {
   };
 
   getTicker = async () => {
-    if (this.state.currencyCode === null) {
+    if (this.props.currency === null) {
       console.log('No currencyCode to update');
       return;
     }
 
-    const infuraConversion = 'eth' + this.state.currencyCode;
-
-    console.log('Fetching results for ' + infuraConversion);
+    const infuraConversion = 'eth' + this.props.currency;
 
     const url = 'https://api.infura.io/v1/ticker/' + infuraConversion;
     try {
@@ -42,8 +28,8 @@ class EthConverter extends React.Component {
           return response.json();
         })
         .then(results => {
-          console.log(JSON.stringify(results));
-          this.setState({ results: results }, () => {
+          // console.log(JSON.stringify(results));
+          this.setState({ results: results, loading: false }, () => {
             this.updateTimeAgo(results.timestamp);
             this.updateConvertedValue();
             this.getCurrencyDetails();
@@ -56,13 +42,6 @@ class EthConverter extends React.Component {
     }
   };
 
-  buildCurrencyTypeList = () => {
-    const currencyTypes = [...new Set(Currencies.map(item => item.type))];
-
-    console.log(currencyTypes);
-    this.setState({ currencyTypes });
-  };
-
   getCurrencyDetails = () => {
     if (this.state.results === null) {
       return;
@@ -70,7 +49,7 @@ class EthConverter extends React.Component {
     const currencyDetails = Currencies.filter(
       currency => currency.code === this.state.results.quote.toLowerCase(),
     );
-    console.log(currencyDetails[0]);
+    // console.log(currencyDetails[0]);
     this.setState({ currencyDetails: currencyDetails[0] });
 
     return currencyDetails[0];
@@ -80,7 +59,7 @@ class EthConverter extends React.Component {
     if (this.state.results === null) {
       return false;
     }
-    const convertedValue = this.state.baseValue * this.state.results.bid;
+    const convertedValue = this.props.value * this.state.results.bid;
     this.setState({ convertedValue });
     return convertedValue;
   };
@@ -121,59 +100,9 @@ class EthConverter extends React.Component {
     return timeAgoString;
   };
 
-  onCurrencyTypeSelected = e => {
-    e.preventDefault();
-    // find currencies that match and create new array of only name and code
-    // Need to wait for rimble-ui version that supports this - 0.9.5
-    // const currencyDropdown = Currencies.filter(currency => currency.type === e.target.value).map(currency => ({label: currency.name, value: currency.code}));
-    const currencyDropdown = [
-      ...new Set(
-        Currencies.filter(currency => currency.type === e.target.value).map(
-          currency => currency.code,
-        ),
-      ),
-    ];
-    console.log('currencyDropdown', currencyDropdown);
-    this.setState({ currencyDropdown, results: null, currencyCode: null });
-  };
-
-  onCurrencySelected = e => {
-    e.preventDefault();
-
-    const currencyCode = e.target.value;
-    console.log('Currency code: ', currencyCode);
-    this.setState({ currencyCode }, () => {
-      this.getTicker();
-    });
-  };
-
-  onInputChange = e => {
-    e.preventDefault();
-    console.log('baseValue', e.target.value);
-    this.setState({ baseValue: e.target.value }, () => {
-      this.updateConvertedValue();
-    });
-  };
-
-  populateDropdown = async () => {
-    const values = await fetch('https://api.infura.io/v1/ticker/symbols')
-      .then(response => {
-        return response.json();
-      })
-      .then(results => {
-        return results.symbols;
-      });
-
-    return values;
-  };
-
   async componentDidMount() {
-    const values = await this.populateDropdown();
-    console.log('values', values);
-    this.setState({ conversions: values }, () => {
-      this.buildCurrencyTypeList();
-      this.setState({ loading: false });
-    });
+    console.log('Initial mount', this.props.currency, this.props.value);
+    this.getTicker();
 
     this.interval = setInterval(() => this.updateTimeAgo(), 1000);
     this.tickerInterval = setInterval(() => this.getTicker(), 60000);
@@ -184,105 +113,39 @@ class EthConverter extends React.Component {
     clearInterval(this.tickerInterval);
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.currency !== this.props.currency) {
+      console.log('Fetching new currency', this.props.currency);
+      this.getTicker();
+    } else if (prevProps.value !== this.props.value) {
+      console.log('Calculating new value', this.props.value);
+      this.updateConvertedValue();
+    }
+  }
+
   render() {
     return (
-      <Box className="App" m={5}>
-        <Heading.h1>Infura Conversion API</Heading.h1>
-        {this.state.loading === false ? (
-          <Flex alignItems={'center'} mt={4} mx={'auto'}>
-            <Field label={'Eth Amount'} mr={4}>
-              <Input
-                type="number"
-                required
-                value={this.state.baseValue}
-                onChange={this.onInputChange}
-              />
-            </Field>
-            <Field label={'Currency Type'} mr={4}>
-              <Select
-                name="type"
-                onChange={this.onCurrencyTypeSelected}
-                value={this.state.currentCurrencyType}
-                items={this.state.currencyTypes}
-                required
-              />
-            </Field>
-            {this.state.currencyDropdown && (
-              <Field label={'Currency'} mr={4}>
-                <Select
-                  name="symbol"
-                  onChange={this.onCurrencySelected}
-                  value={this.state.convert}
-                  items={this.state.currencyDropdown}
-                  required
-                />
-              </Field>
-            )}
-
-            {this.state.results && (
-              <Heading.h3 pt={2}>
+      <Box>
+        {this.state.loading === false
+          ? this.state.results && (
+              <Tooltip
+                message={
+                  this.state.lastUpdated +
+                  ' from ' +
+                  this.state.results.exchange
+                }
+              >
                 {this.state.currencyDetails && (
-                  <Text.span>
+                  <>
                     {String.fromCharCode(
                       this.state.currencyDetails.unicodeDecimal,
                     )}
-                  </Text.span>
+                  </>
                 )}{' '}
                 {this.state.convertedValue} {this.state.results.quote}
-              </Heading.h3>
-            )}
-          </Flex>
-        ) : (
-          'Loading...'
-        )}
-
-        {this.state.convert && <div>Value: {this.state.convert}</div>}
-
-        {this.state.results && (
-          <Card>
-            <Heading.h2>Conversion Details</Heading.h2>
-            <Flex mt={3}>
-              <Text width={'100px'}>Base:</Text>{' '}
-              <Text>{this.state.results.base}</Text>
-            </Flex>
-            <Flex>
-              <Flex />
-              <Text width={'100px'}>Quote:</Text>{' '}
-              <Text>{this.state.results.quote}</Text>
-            </Flex>
-            <Flex>
-              <Text width={'100px'}>Bid:</Text>{' '}
-              <Text>
-                {this.state.currencyDetails && (
-                  <Text.span>
-                    {String.fromCharCode(
-                      this.state.currencyDetails.unicodeDecimal,
-                    )}
-                  </Text.span>
-                )}
-                {this.state.results.bid}
-              </Text>
-            </Flex>
-            <Flex>
-              <Text width={'100px'}>Ask:</Text>{' '}
-              <Text>
-                {this.state.currencyDetails && (
-                  <Text.span>
-                    {String.fromCharCode(
-                      this.state.currencyDetails.unicodeDecimal,
-                    )}
-                  </Text.span>
-                )}
-                {this.state.results.ask}
-              </Text>
-            </Flex>
-            <Flex>
-              <Text width={'100px'}>Exchange:</Text>{' '}
-              <Text>{this.state.results.exchange}</Text>
-            </Flex>
-            <Text>{this.state.lastUpdated}</Text>
-          </Card>
-        )}
+              </Tooltip>
+            )
+          : 'Loading...'}
       </Box>
     );
   }
